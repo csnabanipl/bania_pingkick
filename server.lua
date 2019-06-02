d587a845c71e5cb118bcdeb3e956a54b4a2250eb
csnabani <39969402+csnabanipl@users.noreply.github.com>
2019-06-02 17:14:09 +0200

    Final

--- server.lua
+++ server.lua
@@ -1,18 +1,18 @@
 local ImmunitetIds = {
-	-- 'steam:11000010076d282',
-	-- 'steam:11000010076d282'
-}
+    -- 'steam:11000010076d282',
+    -- 'steam:11000010076d282'
+    }
 
 local MaxPing = 120
 
 function SteamIdInList(id)
-   for k, v in pairs(ImmunitetIds) do 
-      if (id == v) then 
-	  return true 
-      end 
-   end
+    for k, v in pairs(ImmunitetIds) do
+        if (id == v) then
+            return true
+        end
+    end
 
-   return false 
+    return false
 end
 
 RegisterServerEvent('checkPing')
@@ -21,10 +21,10 @@ AddEventHandler('checkPing', function()
     local ping = GetPlayerPing(source)
 
     if ping >= MaxPing then
-	if (SteamIdInList(id)) then
-	    TriggerClientEvent('chatMessage', source, 'PING KICKER ', { 255, 0, 0 }, 'Your Ping is too high!')
-	else
-	    DropPlayer(source, 'Your Ping is too high (Ping Limit: ' .. MaxPing .. ' Your Ping: ' .. ping .. ')')
-	end	
+        if (SteamIdInList(id)) then
+            TriggerClientEvent('chatMessage', source, 'PING KICKER ', { 255, 0, 0 }, 'Your Ping is too high!')
+        else
+            DropPlayer(source, 'Your Ping is too high (Ping Limit: ' .. MaxPing .. ' Your Ping: ' .. ping .. ')')
+        end
     end
 end)
